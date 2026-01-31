2e6b0d8c0f523adb3cd4c2eb796b59b3c5997bdf
Jerry Wang <jew054@ucsd.edu>
2026-01-31 13:16:18 -0800

    Update week1_exercise2.cpp

--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -1,14 +1,22 @@
-//STARTER 
-//THIS EXERCISE IS TO BE DONE IN THE ARM MBED SIMULATOR
-//http://195.130.59.221/
-//IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
-int main() {
-    while (1) {
-        printf("Week 1 Exercise 2");
+AnalogIn potential(p15);
+DigitalOut led(p5);
 
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+int main(){
+    int time = 2000;
+    while(1){
+        double percent = potential.read();
+        double on = percent * time;
+        double off = time - on;
+        
+        if(on>0){
+            led = 1;
+            wait_ms(on);
+        }
+        if(off>0){
+            led = 0;
+            wait_ms(off);
+        }
     }
 }
